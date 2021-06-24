60f59ad89efffcfdd2d85ddf9f86e2c1952b9ac4
ellietoulabi <elitoulabin@gmail.com>
2021-06-24 16:16:47 -0700

    Form Parser added

--- crawler.py
+++ crawler.py
@@ -3,52 +3,90 @@ import logging
 from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-logging.basicConfig(
-    format='%(message)s',
-    level=logging.INFO)
-
+logging.basicConfig(format="%(message)s", level=logging.INFO)
 
 
-visited = []                                            # visited urls
-not_visited = []                                        # urls found but still not visited
-banned_keywords = []                                    # keywords like logout
+visited = []  # visited urls
+not_visited = []  # urls found but still not visited
+banned_keywords = []  # keywords like logout
 
 
 def _crawl(url):
 
-    linked=[]
-    html=requests.get(url).text
-    soup = BeautifulSoup(html, 'html.parser')
-    for link in soup.find_all('a'):
-        path = link.get('href')
-        if path and path.startswith('/'):
+    linked = []
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html.parser")
+    for link in soup.find_all("a"):
+        path = link.get("href")
+        if path:  # and path.startswith('/'):
             path = urljoin(url, path)
             linked.append(path)
     for u in linked:
         if u not in visited and u not in not_visited:
             not_visited.append(u)
-        
+
 
 def _crawling():
 
-    print('[+] Crawling Started...\n')
+    print("[+] Crawling Started...\n")
     while not_visited:
         url = not_visited.pop(0)
-        logging.info(f'Crawling: {url}')
+        logging.info(f"Crawling: {url}")
         try:
             _crawl(url)
         except Exception:
-            logging.exception(f'Failed to crawl: {url}')
+            logging.exception(f"Failed to crawl: {url}")
         finally:
             visited.append(url)
-            with open('urls.csv', 'a') as file:
+            with open("urls.csv", "a") as file:
                 file.write(f"{url}\n")
     print("Crawling Done. Results Saved in [urls.csv].")
 
 
-if __name__ == '__main__':
+def _parse_form_tags():
 
-    init_url='https://www.imdb.com/'
+    with open("urls.csv", newline="\n") as csvfile:
+        urls = list(csv.reader(csvfile))
+
+    form_data = {"data": []}
+    
+    for url in urls:
+        html = requests.get(url[0]).text
+        soup = BeautifulSoup(html, "html.parser")
+        for form in soup.find_all("form"):
+            
+            form_dict = {}
+            inputs_list = []
+            
+            action = form.get("action")
+            method = form.get("method")
+            inputs = form.find_all("input")
+
+            form_dict["action"] = action
+            form_dict["method"] = method
+            form_dict["url"] = url
+
+            with open("form-details.csv", "a") as file:
+                file.write(f"{url},{method},{action},")
+                for _i in inputs:
+                    stripped = str(_i).replace("\n", " ")
+                    inputs_list.append(stripped)
+
+                    file.write(f"{stripped},")
+
+                file.write("\n")
+
+            form_dict["inputs"] = inputs_list
+            form_data["data"].append(form_dict)
+
+    return form_data
+
+
+if __name__ == "__main__":
+
+    init_url = "https://google.com/"
     not_visited.append(init_url)
-    _crawling()
+    # _crawling()
+    forms = _parse_form_tags()
