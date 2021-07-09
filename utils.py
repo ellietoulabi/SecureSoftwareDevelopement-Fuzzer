133b7d11b67abf27f0b975d560afa838911ee82e
Mohammad Mirsafaei <info@mirsafaei.ir>
2021-07-09 06:58:28 +0430

    utils update

--- utils.py
+++ utils.py
@@ -1,2 +1,2 @@
 def colored(r, g, b, text):
-    return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
+    return "\033[38;2;{};{};{}m{} \033[38;2;0;0;0m".format(r, g, b, text)
